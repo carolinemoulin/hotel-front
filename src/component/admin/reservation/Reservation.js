a0cb404de2cdc83dba3700a7573acabd45cd68de
Caroline Moulin <40388309+Ilvedia@users.noreply.github.com>
2020-06-29 12:35:14 +0200

    affichage des réservations

--- src/component/admin/reservation/Reservation.js
+++ src/component/admin/reservation/Reservation.js
@@ -1,39 +1,85 @@
-import React, { Component } from 'react';
-import FetchData from './../../../services/FetchData';
+import React, { Component } from "react";
+import FetchData from "./../../../services/FetchData";
 
 class Reservation extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      reservations: []
+      reservations: [],
+      error: false,
     };
     this.fd = new FetchData(); //singleton
   }
 
   successReservation = (data) => {
-    console.log('Dans successReservation');
+    console.log("Dans successReservation");
     // copie du state
-    const copy_state = {...this.state};
+    const copy_state = { ...this.state };
     // modification de la copie du state
     copy_state.reservations = data;
     // sauvegarde du state
     this.setState(copy_state);
-  }
+  };
 
   failedReservation = (error) => {
-    console.log('Dans failedReservation', error);
-  }
-  
+    console.log("Dans failedReservation", error);
+    // copie du state
+    const copy_state = { ...this.state };
+    // modification de la copie du state
+    copy_state.error = error;
+    // sauvegarde du state
+    this.setState(copy_state);
+  };
+
   componentDidMount = () => {
     // Tentative de récupération des données
-    console.log('dans didmont');
-    
     this.fd.getReservations(this.successReservation, this.failedReservation);
-  }
+  };
 
   render() {
     return (
-      <h2>Réservations</h2>
+      <main className="container">
+        <div className="row">
+          <h2>Réservations</h2>
+        </div>
+        <div className="row">
+          <div className="col">
+            {this.state.error && (
+              <div>
+                <h2>Erreur</h2>
+                <p>Code de l'erreur : {this.state.error.message}</p>
+                <p>Merci de contacter l'administrateur admin@hotel.com</p>
+              </div>
+            )}
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>ID</th>
+                  <th>Catégorie</th>
+                  <th>Date de début</th>
+                  <th>Date de fin</th>
+                  <th>Nombre de personnes</th>
+                  <th>Nombre de nuits</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.reservations.map((reservation) => {
+                  return (
+                    <tr key={reservation.id}>
+                      <td>{reservation.id}</td>
+                      <td>{reservation.categoryId}</td>
+                      <td>{reservation.startDate}</td>
+                      <td>{reservation.endDate}</td>
+                      <td>{reservation.data.persons}</td>
+                      <td>{reservation.data.nights}</td>
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </main>
     );
   }
 }
