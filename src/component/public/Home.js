6388c6a8c281497aacbf10208405f97520c2ccee
Caroline Moulin <40388309+Ilvedia@users.noreply.github.com>
2020-06-30 12:18:35 +0200

    Mise en forme formulaire

--- src/component/public/Home.js
+++ src/component/public/Home.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Header from "../common/Header";
 import "./../css/Home.css";
 import FetchData from './../../services/FetchData';
+import { Form, Button } from 'react-bootstrap';
+import { FaParking, FaWifi, FaCoffee, FaGlassMartiniAlt, FaSnowflake} from 'react-icons/fa';
 
 class Home extends Component {
   constructor(props) {
@@ -32,58 +34,65 @@ class Home extends Component {
       
     }
     
-    
-    
-
-    
   };
   render() {
     return (
       <div className="App">
         <Header path="/" />
         <div className="container">
-          <div className="row">
-            <div className="col">
+          <div className="row headband">
+            <div className="col-md-8">
+
+            </div>
+            <div className="col-md-4">
               <h2>Réservation</h2>
-              <form onSubmit={this.handleSubmit} className="form-group">
-                <div className="form-row">
-                  <div className="form-group col-6">
-                    <label htmlFor="start-date">
+              <Form onSubmit={this.handleSubmit}>
+                <Form.Row>
+                  <Form.Group className="col-6">
+                    <Form.Label htmlFor="start-date">
                       Date d'arrivée :
-                      <input required className="form-control" id="start-date" type="date" defaultValue={""} />
-                    </label>
-                  </div>
-                  <div className="form-group col-6">
-                    <label htmlFor="end-date">
+                      <Form.Control required className="form-control" id="start-date" type="date" defaultValue={""} />
+                    </Form.Label>
+                  </Form.Group>
+                  <Form.Group className="col-6">
+                    <Form.Label htmlFor="end-date">
                       Date de départ :
-                      <input required className="form-control" id="end-date" type="date" defaultValue={""} />
-                    </label>
-                  </div>
-                </div>
-                <div className="form-row">
-                  <div className="form-group col-6">
-                    <label htmlFor="nb-person">
+                      <Form.Control required className="form-control" id="end-date" type="date" defaultValue={""} />
+                    </Form.Label>
+                  </Form.Group>
+                </Form.Row>
+                <Form.Row>
+                  <Form.Group className="col-6">
+                    <Form.Label htmlFor="nb-person">
                       Nombre de personnes :
-                      <input required className="form-control" id="nb-person" type="number" min="1" max="3" />
-                    </label>
-                  </div>
-                  <div className="form-group col-6">
-                    <label htmlFor="category">
+                      <Form.Control required className="form-control" id="nb-person" type="number" min="1" max="3" />
+                    </Form.Label>
+                  </Form.Group>
+                  <Form.Group className="col-6">
+                    <Form.Label htmlFor="category">
                       Catégorie de chambre :
-                      <div>
-                        <select required id="category">
+                      <Form.Control as="select">
                           <option value="1">1 - Chambre simple</option>
                           <option value="2">2 - Chambre double</option>
                           <option value="3">3 - Chambre double - lits séparés</option>
                           <option value="4">4 - Chambre triple - 1 lit double - 1 lit simple</option>
-                        </select>
-                      </div>
-                    </label>
-                  </div>
-                </div>
-                <input className="btn btn-primary" type="submit" value="Envoyer" />
-              </form>
+                      </Form.Control>
+                    </Form.Label>
+                  </Form.Group>
+                </Form.Row>
+                <Button variant="primary" type="submit">Envoyer</Button>
+              </Form>
             </div>
+          </div>
+          <div className="row">
+            <ul className="list-unstyled col-12 bg-primary d-flex flex-row justify-content-around">
+              <li className="p-2"><FaParking></FaParking> Parking</li>
+              <li className="p-2"><FaWifi></FaWifi> WIFI/Accés Internet</li>
+              <li className="p-2"><FaCoffee></FaCoffee> Petit-déjeuner</li>
+              <li className="p-2"><FaGlassMartiniAlt></FaGlassMartiniAlt> Bar</li>
+              <li className="p-2">Restaurant</li>
+              <li className="p-2"><FaSnowflake></FaSnowflake>Air Conditionné</li>
+            </ul>
           </div>
         </div>
       </div>
